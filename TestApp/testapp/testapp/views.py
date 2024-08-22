5292521363558a7573a8437dd6662d201c3467fe
Kalana Wijenayake <kalanatd@gmail.com>
2024-08-22 08:17:02 +0530

    feat [hotfix]: change the display text in the django app

--- TestApp/testapp/testapp/views.py
+++ TestApp/testapp/testapp/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def hello_world(request):
-    return HttpResponse('Hello World - This is changed', status=200)
+    return HttpResponse('Hello World - Interview', status=200)
 
 
 def health_check(request):
